641cef807aaeee4501352720d3f580f9b62dfccf
lwindhorst <lw@4so.de>
2022-12-07 10:00:15 +0100

    Day 1

--- 2022/src/bin/01/main.rs
+++ 2022/src/bin/01/main.rs
@@ -1,3 +1,47 @@
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+#[cfg(target_os = "windows")]
+mod new_line {
+    pub const SINGLE: &str = "\r\n";
+    pub const DOUBLE: &str = "\r\n\r\n";
+}
+
+#[cfg(target_os = "linux")]
+mod new_line {
+    pub const SINGLE: &str = "\n";
+    pub const DOUBLE: &str = "\n\n";
+}
+
 fn main() {
-    println!("Hello World!");
+    let path = PathBuf::from("src/bin/01/input.txt");
+    let input = read_to_string(path).unwrap();
+
+    part_one(&input);
+    part_two(&input);
+}
+
+fn part_one(input: &str) {
+    let answer: u32 = input
+        .split(new_line::DOUBLE)
+        .map(|i| i.split(new_line::SINGLE).filter_map(|s| s.parse::<u32>().ok()).sum())
+        .max().unwrap();
+
+    println!("Part 1: {:?}", answer);
+}
+
+fn part_two(input: &str) {
+    let mut sums: Vec<u32> = input
+        .split(new_line::DOUBLE)
+        .map(|i| i.split(new_line::SINGLE).filter_map(|s| s.parse::<u32>().ok()).sum())
+        .collect();
+
+    sums.sort_by(|a, b| b.cmp(a));
+
+    let mut answer = 0;
+    for i in 0..3 {
+        answer += sums[i];
+    };
+
+    println!("Part 2: {:?}", answer);
 }
