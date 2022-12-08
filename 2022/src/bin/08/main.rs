7a6a0a4e94242c39cf8e171bc314edc9409e7630
lwindhorst <lw@4so.de>
2022-12-08 09:02:14 +0100

    Add dirs for remaining days

---
+++ 2022/src/bin/08/main.rs
@@ -0,0 +1,36 @@
+#![allow(dead_code)]
+
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+#[cfg(target_os = "windows")]
+mod new_line {
+    pub const SINGLE: &str = "\r\n";
+    pub const DOUBLE: &str = "\r\n\r\n";
+}
+
+#[cfg(target_os = "linux")]
+mod new_line {
+    pub const SINGLE: &str = "\n";
+    pub const DOUBLE: &str = "\n\n";
+}
+
+fn main() {
+    let path = PathBuf::from("src/bin/08/input.txt");
+    let input = read_to_string(path).unwrap();
+
+    part_one(&input);
+    part_two(&input);
+}
+
+fn part_one(input: &str) {
+    let answer = input;
+
+    println!("Part 1: {:?}", answer);
+}
+
+fn part_two(input: &str) {
+    let answer = input;
+
+    println!("Part 2: {:?}", answer);
+}
