4221551c0e92add51de547e980d08672dc86746a
Leon Windhorst <leon@windhorst.dev>
2025-12-01 12:30:51 +0100

    Add solution for day 1, 2025

--- 2025/src/01/main.cpp
+++ 2025/src/01/main.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <unordered_map>
 
 int
 main()
@@ -11,10 +10,44 @@ main()
 
     input.open("src/01/input.txt");
 
+    unsigned int count = 0;
+    int position = 50;
+
     while (std::getline(input, line))
     {
-        std::cout << line << std::endl;
+        int direction = line[0] == 'L' ? -1 : 1;
+        int distance = std::stoi(line.substr(1));
+
+        if (line[0] == 'L')
+        {
+            if (position == 0)
+            {
+                count += distance / 100;
+            }
+            else if (distance >= position)
+            {
+                count += 1 + (distance - position) / 100;
+            }
+        }
+        else if (line[0] == 'R')
+        {
+            if (position == 0)
+            {
+                count += distance / 100;
+            }
+            else if (distance >= 100 - position)
+            {
+                count += 1 + (distance - (100 - position)) / 100;
+            }
+        }
+
+        position += direction * distance;
+        position %= 100;
+        if (position < 0)
+            position += 100;
     }
+
+    std::cout << count << std::endl;
 
     return 0;
 }
