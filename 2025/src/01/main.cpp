138295171a1b4cfa3279cc49e8359790be2b5936
Leon Windhorst <leon@windhorst.dev>
2025-12-01 15:04:46 +0100

    Cleanup solution for day 1, 2025

--- 2025/src/01/main.cpp
+++ 2025/src/01/main.cpp
@@ -2,20 +2,52 @@
 #include <iostream>
 #include <string>
 
+#define INPUT_PATH "src/01/input.txt"
+
 int
-main()
+part_one()
 {
     std::ifstream input;
     std::string line;
 
-    input.open("src/01/input.txt");
+    unsigned int count = 0;
+    int position = 50;
+
+    input.open(INPUT_PATH);
+
+    while (std::getline(input, line))
+    {
+
+        int direction = line[0] == 'L' ? -1 : 1;
+        int distance = std::stoi(line.substr(1));
+
+        position += direction * distance;
+        position %= 100;
+        if (position < 0)
+            position += 100;
+
+        if (position == 0)
+            ++count;
+    }
+
+    input.close();
+
+    return count;
+}
+
+int
+part_two()
+{
+    std::ifstream input;
+    std::string line;
 
     unsigned int count = 0;
     int position = 50;
 
+    input.open(INPUT_PATH);
+
     while (std::getline(input, line))
     {
-        int direction = line[0] == 'L' ? -1 : 1;
         int distance = std::stoi(line.substr(1));
 
         if (line[0] == 'L')
@@ -28,6 +60,10 @@ main()
             {
                 count += 1 + (distance - position) / 100;
             }
+
+            position = (position - distance) % 100;
+            if (position < 0)
+                position += 100;
         }
         else if (line[0] == 'R')
         {
@@ -39,15 +75,21 @@ main()
             {
                 count += 1 + (distance - (100 - position)) / 100;
             }
-        }
 
-        position += direction * distance;
-        position %= 100;
-        if (position < 0)
-            position += 100;
+            position = (position + distance) % 100;
+        }
     }
 
-    std::cout << count << std::endl;
+    input.close();
+
+    return count;
+}
+
+int
+main()
+{
+    std::cout << "part one: " << part_one() << std::endl;
+    std::cout << "part two: " << part_two() << std::endl;
 
     return 0;
 }
